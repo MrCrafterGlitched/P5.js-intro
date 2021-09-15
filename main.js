6a8f76f3acee3aac427649fa8cc8717658059e3b
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-09-15 17:03:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ function setup() {
 }
 function draw() {
     image(video,230,150,220,200)
-    
-    fill(255,0,0)
-    stroke(255,0,0)
-    circle(50,50,80)
-    
     fill(0,128,0)
     stroke(0,128,0)
     rect(90,40,460,20)
-    
+  rect(70,60,20,370)
+  rect(560,60,20,370)
     rect(90,420,460,20)
+    fill(255,0,0)
+    stroke(255,0,0)
+    circle(50,50,80)
+    circle(590,50,80)
+    circle(50,430,80)
+    circle(590,430,80)
 }
